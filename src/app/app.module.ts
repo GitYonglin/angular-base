81987df2acd12b6dfc65a5007feac58a95f89b3b
Peach <peachlin@outlook.com>
2018-03-28 15:12:43 +0800

    模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
+  // 放置组件（component），管道（pipe）
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
+  // 放置模块（module）
   imports: [
     BrowserModule
   ],
+  // 放置服务（service）
   providers: [],
+  // 放置动态组件
+  entryComponents: [
+    UserListComponent
+  ],
+  // 启动项
   bootstrap: [AppComponent]
 })
 export class AppModule { }
