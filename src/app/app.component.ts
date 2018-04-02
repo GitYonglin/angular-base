24f0111e106a28e33f7d58d13d7e23adf7d46590
Peach <peachlin@outlook.com>
2018-04-02 13:46:19 +0800

    响应式表单

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegisterModel } from './model/register-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public regModel = new RegisterModel();
+export class AppComponent implements OnInit {
+  public userForm: FormGroup;
+  public userInfo = new RegisterModel();
+  // 数据key
+  public formErrors = {
+    'userName': ''
+  };
+  // 验证提示数据
+  validationMessages = {
+    'userName': {
+      'required': '用户名必须输入。',
+      'minlength': '用户名4到32个字符。'
+    }
+  };
+
+  constructor(public fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+  // 数据验证提示
+  onValueChanged(data?: any) {
+    if (!this.userForm) {
+      return;
+    }
+    const form = this.userForm;
+    // tslint:disable-next-line:forin
+    for (const field in this.formErrors) {
+      console.log(field);
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+  // 数据绑定与验证规则设置
+  buildForm(): void {
+    this.userForm = this.fb.group({
+      'userName': [
+        this.userInfo.userName,
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(32)
+        ]
+      ],
+    });
+    this.userForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChineseMobileValidator } from './validation/Chinese-mobile-validator.directive';
@@ -14,7 +13,8 @@ import { ChineseMobileValidator } from './validation/Chinese-mobile-validator.di
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
